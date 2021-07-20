38c2efd106a023233bec59aebe90a0ca5af561b3
Seb Kay <seb@sebkay.com>
2021-07-20 17:49:45 +0100

    Remove useless comments

--- src/PostTypes/Post.php
+++ src/PostTypes/Post.php
@@ -5,16 +5,52 @@ namespace OOPWP\PostTypes;
 class Post
 {
     protected $id;
-    protected $WP_Post;
-    protected $title;
-    protected $url;
-    protected $slug;
-    protected $content;
-    protected $status;
-    protected $format;
-    protected $excerpt;
-    protected $date;
-    protected $dateModified;
+
+    /**
+     * @var string
+     */
+    protected $title = '';
+
+    /**
+     * @var string
+     */
+    protected $url = '';
+
+    /**
+     * @var string
+     */
+    protected $slug = '';
+
+    /**
+     * @var string
+     */
+    protected $content = '';
+
+    /**
+     * @var string
+     */
+    protected $status = '';
+
+    /**
+     * @var string
+     */
+    protected $format = '';
+
+    /**
+     * @var string
+     */
+    protected $excerpt = '';
+
+    /**
+     * @var string
+     */
+    protected $date = '';
+
+    /**
+     * @var string
+     */
+    protected $dateModified = '';
+
     protected $parent;
 
     /**
@@ -24,118 +60,71 @@ class Post
      */
     public function __construct(int $id)
     {
-        $this->id      = $id ?: -1;
-        $this->WP_Post = \get_post($this->id());
+        $this->id = $id ?: -1;
+        $WP_Post  = \get_post($this->id());
 
-        if (!$this->WP_Post) {
+        if (!$WP_Post) {
             return;
         }
 
-        $this->title         = \get_the_title($this->WP_Post);
-        $this->url           = \get_permalink($this->WP_Post);
-        $this->slug          = $this->WP_Post->post_name;
-        $this->content       = $this->WP_Post->post_content;
-        $this->status        = $this->WP_Post->post_status;
-        $this->format        = \get_post_format($this->WP_Post) ?: 'standard';
-        $this->excerpt       = \get_the_excerpt($this->WP_Post);
-        $this->date          = $this->WP_Post->post_date;
-        $this->dateModified  = $this->WP_Post->post_modified;
-        $this->parent        = new self($this->WP_Post->post_parent);
+        $this->title        = \get_the_title($WP_Post);
+        $this->url          = \get_permalink($WP_Post);
+        $this->slug         = $WP_Post->post_name;
+        $this->content      = $WP_Post->post_content;
+        $this->status       = $WP_Post->post_status;
+        $this->format       = \get_post_format($WP_Post) ?: 'standard';
+        $this->excerpt      = \get_the_excerpt($WP_Post);
+        $this->date         = $WP_Post->post_date;
+        $this->dateModified = $WP_Post->post_modified;
+        $this->parent       = new self($WP_Post->post_parent);
     }
 
-    /**
-     * The ID
-     *
-     * @return int
-     */
     public function id()
     {
         return $this->id;
     }
 
-    /**
-     * The url
-     *
-     * @return string
-     */
     public function url()
     {
         return $this->url;
     }
 
-    /**
-     * The slug
-     *
-     * @return string
-     */
     public function slug()
     {
         return $this->slug;
     }
 
-    /**
-     * The status
-     *
-     * @return string
-     */
     public function status()
     {
         return $this->status;
     }
 
-    /**
-     * The format
-     *
-     * @return string
-     */
     public function format()
     {
         return $this->format;
     }
 
-    /**
-     * The title
-     *
-     * @return string
-     */
     public function title()
     {
         return $this->title;
     }
 
-    /**
-     * The excerpt
-     *
-     * @return string
-     */
     public function excerpt()
     {
         return $this->excerpt;
     }
 
-    /**
-     * The publish date
-     *
-     * @return string
-     */
     public function publishDate()
     {
         return $this->date;
     }
 
-    /**
-     * The modified date
-     *
-     * @return string
-     */
     public function modifiedDate()
     {
         return $this->dateModified;
     }
 
     /**
-     * The content
-     *
      * @return string
      */
     public function content()
@@ -143,11 +132,6 @@ class Post
         return \apply_filters('the_content', $this->content);
     }
 
-    /**
-     * Parent Post object
-     *
-     * @return Post
-     */
     public function parent()
     {
         return $this->parent;

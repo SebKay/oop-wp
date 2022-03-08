33dbb6ea5afffaaa4bbf1c4389a4b1f13783dba4
Seb Kay <seb@fhoke.com>
2022-03-08 16:01:31 +0000

    Refactor so all elements of a Post can be loaded individually, not always all at once during class construction

--- src/PostTypes/Post.php
+++ src/PostTypes/Post.php
@@ -10,11 +10,6 @@ class Post
 
     protected $WP_Post;
 
-    protected $options = [
-        'thumbnail_size'    => 'post-thumbnail',
-        'thumbnail_classes' => '',
-    ];
-
     public string $title   = '';
     public string $url     = '';
     public string $slug    = '';
@@ -22,43 +17,114 @@ class Post
     public string $status  = '';
     public string $format  = '';
     public string $excerpt = '';
-    public Carbon $date;
-    public Carbon $dateModified;
+    public Carbon $publishedDate;
+    public Carbon $modifiedDate;
     public \WP_Post $parent;
-    public string $thumbnail = '';
+    public string $thumbnailHtml = '';
 
-    /**
-     * Set up
-     *
-     * @param integer $id
-     */
-    public function __construct(int $id)
+    public function __construct(int $id = 0)
     {
-        $this->id      = $id ?: 0;
         $this->WP_Post = \get_post($this->id);
-
-        if (!$this->WP_Post) {
-            return;
-        }
-
-        $this->title        = \get_the_title($this->WP_Post);
-        $this->url          = \get_permalink($this->WP_Post);
-        $this->slug         = $this->WP_Post->post_name;
-        $this->content      = \apply_filters('the_content', $this->WP_Post->post_content);
-        $this->status       = $this->WP_Post->post_status;
-        $this->format       = \get_post_format($this->WP_Post) ?: 'standard';
-        $this->excerpt      = \get_the_excerpt($this->WP_Post);
-        $this->date         = new Carbon($this->WP_Post->post_date);
-        $this->dateModified = new Carbon($this->WP_Post->post_modified);
-        $this->parent       = new self($this->WP_Post->post_parent);
     }
 
-    public function withThumbnail()
+    public function exists()
     {
-        $this->thumbnail = \get_the_post_thumbnail(
+        return $this->WP_Post ? true : false;
+    }
+
+    public function withAll()
+    {
+        $this
+            ->withTitle()
+            ->withUrl()
+            ->withSlug()
+            ->withContent()
+            ->withStatus()
+            ->withFormat()
+            ->withExcerpt()
+            ->withPublishedDate()
+            ->withModifiedDate()
+            ->withParent();
+
+        return $this;
+    }
+
+    public function withTitle()
+    {
+        $this->title = \get_the_title($this->WP_Post);
+
+        return $this;
+    }
+
+    public function withUrl()
+    {
+        $this->url = \get_permalink($this->WP_Post);
+
+        return $this;
+    }
+
+    public function withSlug()
+    {
+        $this->slug = $this->WP_Post->post_name;
+
+        return $this;
+    }
+
+    public function withContent()
+    {
+        $this->content = \apply_filters('the_content', $this->WP_Post->post_content);
+
+        return $this;
+    }
+
+    public function withExcerpt()
+    {
+        $this->excerpt = \get_the_excerpt($this->WP_Post);
+
+        return $this;
+    }
+
+    public function withStatus()
+    {
+        $this->status = $this->WP_Post->post_status;
+
+        return $this;
+    }
+
+    public function withFormat()
+    {
+        $this->format = \get_post_format($this->WP_Post) ?: 'standard';
+
+        return $this;
+    }
+
+    public function withPublishedDate()
+    {
+        $this->publishedDate = new Carbon($this->WP_Post->post_date);
+
+        return $this;
+    }
+
+    public function withModifiedDate()
+    {
+        $this->modifiedDate = new Carbon($this->WP_Post->post_modified);
+
+        return $this;
+    }
+
+    public function withParent()
+    {
+        $this->parent = new self($this->WP_Post->post_parent);
+
+        return $this;
+    }
+
+    public function withThumbnail(string $size = 'post-thumbnail', string $css_classes = '')
+    {
+        $this->thumbnailHtml = \get_the_post_thumbnail(
             $this->WP_Post,
-            $this->options['thumbnail_size'],
-            $this->options['thumbnail_classes']
+            $size,
+            $css_classes
         );
 
         return $this;

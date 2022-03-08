d9755e9c476caa02772e4924d93bd0b7812375fb
Seb Kay <seb@fhoke.com>
2022-03-08 15:50:17 +0000

    Remove all methods from Post that are no longer needed due to properties being public

--- src/PostTypes/Post.php
+++ src/PostTypes/Post.php
@@ -15,15 +15,15 @@ class Post
         'thumbnail_classes' => '',
     ];
 
-    public string $title = '';
-    public string $url = '';
-    public string $slug = '';
+    public string $title   = '';
+    public string $url     = '';
+    public string $slug    = '';
     public string $content = '';
-    public string $status = '';
-    public string $format = '';
+    public string $status  = '';
+    public string $format  = '';
     public string $excerpt = '';
     public Carbon $date;
-    public Carbon $dateModified = '';
+    public Carbon $dateModified;
     public \WP_Post $parent;
     public string $thumbnail = '';
 
@@ -34,110 +34,33 @@ class Post
      */
     public function __construct(int $id)
     {
-        $this->id      = $id ?: -1;
-        $this->WP_Post = \get_post($this->id());
+        $this->id      = $id ?: 0;
+        $this->WP_Post = \get_post($this->id);
 
         if (!$this->WP_Post) {
             return;
         }
 
-        $this->title        = \get_the_title($this->WPPost());
-        $this->url          = \get_permalink($this->WPPost());
-        $this->slug         = $this->WPPost()->post_name;
-        $this->content      = $this->WPPost()->post_content;
-        $this->status       = $this->WPPost()->post_status;
-        $this->format       = \get_post_format($this->WPPost()) ?: 'standard';
-        $this->excerpt      = \get_the_excerpt($this->WPPost());
-        $this->date         = $this->WPPost()->post_date;
-        $this->dateModified = $this->WPPost()->post_modified;
-        $this->parent       = new self($this->WPPost()->post_parent);
-    }
-
-    public function setOption(string $key, $value)
-    {
-        if (isset($this->options[$key])) {
-            $this->options[$key] = $value;
-        }
-
-        return $this;
+        $this->title        = \get_the_title($this->WP_Post);
+        $this->url          = \get_permalink($this->WP_Post);
+        $this->slug         = $this->WP_Post->post_name;
+        $this->content      = \apply_filters('the_content', $this->WP_Post->post_content);
+        $this->status       = $this->WP_Post->post_status;
+        $this->format       = \get_post_format($this->WP_Post) ?: 'standard';
+        $this->excerpt      = \get_the_excerpt($this->WP_Post);
+        $this->date         = new Carbon($this->WP_Post->post_date);
+        $this->dateModified = new Carbon($this->WP_Post->post_modified);
+        $this->parent       = new self($this->WP_Post->post_parent);
     }
 
     public function withThumbnail()
     {
         $this->thumbnail = \get_the_post_thumbnail(
-            $this->WPPost(),
+            $this->WP_Post,
             $this->options['thumbnail_size'],
             $this->options['thumbnail_classes']
         );
 
         return $this;
-    }
-
-    public function id()
-    {
-        return $this->id;
-    }
-
-    public function WPPost()
-    {
-        return $this->WP_Post;
-    }
-
-    public function url()
-    {
-        return $this->url;
-    }
-
-    public function slug()
-    {
-        return $this->slug;
-    }
-
-    public function status()
-    {
-        return $this->status;
-    }
-
-    public function format()
-    {
-        return $this->format;
-    }
-
-    public function title()
-    {
-        return $this->title;
-    }
-
-    public function excerpt()
-    {
-        return $this->excerpt;
-    }
-
-    public function date()
-    {
-        return new Carbon($this->date);
-    }
-
-    public function modifiedDate()
-    {
-        return new Carbon($this->dateModified);
-    }
-
-    /**
-     * @return string
-     */
-    public function content()
-    {
-        return \apply_filters('the_content', $this->content);
-    }
-
-    public function parent()
-    {
-        return $this->parent;
-    }
-
-    public function thumbnail()
-    {
-        return $this->thumbnail;
     }
 }
